1ad21fd24ca422d2716aeaa65581cde0d3dca261
Khent-bit <khentharold.amancio@cit.edu>
2022-12-17 11:53:59 +0800

    Complete FoodORder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
@@ -16,6 +18,55 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    public FoodOrderGUI(){
+        this.setContentPane(panel1);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setTitle("Food Ordering System");
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double total = 0;
+                if(cPizza.isSelected()){
+                    total += 100;
+                }
+                if(cBurger.isSelected()){
+                    total += 80;
+                }
+                if(cFries.isSelected()){
+                    total += 65;
+                }
+                if(cSoftDrinks.isSelected()){
+                    total += 55;
+                }
+                if(cTea.isSelected()){
+                    total += 50;
+                }
+                if(cSundae.isSelected()){
+                    total += 40;
+                }
+                if(rbNone.isSelected()){
+                    total -= 0;
+                }
+                if(rb5.isSelected()){
+                    double temp = total * .05;
+                    total -= temp;
+                }
+                if(rb10.isSelected()){
+                    double temp = total * .10;
+                    total -= temp;
+                }
+                if(rb15.isSelected()){
+                    double temp = total * .15;
+                    total -= temp;
+                }
+
+                JOptionPane.showMessageDialog(null, String.format("The total price is %.2f", total));
+            }
+        });
+
+    }
     public static void main(String[] args){
         JFrame fr = new FoodOrderGUI();
     }
